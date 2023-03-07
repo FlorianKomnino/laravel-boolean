5664c84603bfb8e2fc3b41f8a46f7c152d547c02
Sebastiano Calella <sebastianocalella97@gmail.com>
2023-03-07 15:59:59 +0100

    implement in router.js main pages list

--- src/router.js
+++ src/router.js
@@ -1,8 +1,40 @@
 import { createRouter, createWebHistory } from 'vue-router';
 
+import HomePage from './pages/HomePage.vue';
+import AboutUs from './pages/AboutUs.vue';
+import ContactUs from './pages/ContactUs.vue';
+import BooksList from './pages/BooksList.vue';
+import SingleBook from './pages/SingleBook.vue';
+
 const router = createRouter({
     history: createWebHistory(),
-    routes: []
+    routes: [
+        {
+            path: '/',
+            name: 'home',
+            component: HomePage
+        },
+        {
+            path: '/about-us',
+            name: 'about-us',
+            component: AboutUs
+        },
+        {
+            path: '/contact_us',
+            name: 'contact-us',
+            component: ContactUs
+        },
+        {
+            path: '/books',
+            name: 'books',
+            component: BooksList
+        },
+        {
+            path: '/books/:slug',
+            name: 'book',
+            component: SingleBook
+        },
+    ]
 });
 
 export { router };
