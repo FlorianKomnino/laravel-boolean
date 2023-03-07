38f4ddd2ae5458f0fdc2582cae6d01a9b598d222
Sebastiano Calella <sebastianocalella97@gmail.com>
2023-03-07 16:08:58 +0100

    ContactUs prototype

---
+++ src/pages/ContactUs.vue
@@ -0,0 +1,14 @@
+<template lang="">
+    <div>
+        <h2>
+            Contact Us
+        </h2>
+    </div>
+</template>
+<script>
+export default {
+    name: 'ContactUs'
+}
+</script>
+<style lang="scss">
+</style>
