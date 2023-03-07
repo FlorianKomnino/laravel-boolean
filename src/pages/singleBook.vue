bc96eaa1b7e3f1e67cacc05b27bf9633b2836a80
Sebastiano Calella <sebastianocalella97@gmail.com>
2023-03-07 16:24:13 +0100

    SingleBook page prototype

---
+++ src/pages/singleBook.vue
@@ -0,0 +1,45 @@
+<template lang="">
+    <div>
+        <SingleBook v-if="book" :book="book" :isShow="true"/>
+    </div>
+</template>
+
+<script>
+import SingleBook from '../components/SingleBook.vue'
+import axios from 'axios'
+
+export default {
+
+    name: 'SingleBook',
+    components: {
+        SingleBook
+    },
+    data() {
+        return {
+            book: null,
+            loading: false,
+            urlAddress: 'http://127.0.0.1:8000',
+        }
+    },
+    methods: {
+        getBook(){
+            axios.get(this.urlAddress + `/api/projects/${this.$route.params.slug}`, {
+                params: {
+                }
+            })
+            .then((response) => {
+                this.book = response.data.results;
+                console.warn(response);
+            })
+            .catch(function (error) {
+                console.warn(error);
+            });
+        },
+    },
+    created() {
+        this.getBook();
+    },
+}
+</script>
+<style lang="scss">
+</style>
